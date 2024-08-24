4e388e2b4cf3bb8ad9f6523155a86100bad26385
Hyper Code <meihkacan381@gmail.com>
2024-08-24 21:17:37 +0300

    Jquery & Font-Awesome Added Local For Use Without Internet Connection

--- django_quiz_app/static/js/all_quizzes.js
+++ django_quiz_app/static/js/all_quizzes.js
@@ -2,7 +2,7 @@ $(function(){
     var all_quizzes_html = "";
     $.get("/all_quizzes_api",function(data,status){
         data.forEach(function(all_quizzes_datas){
-            all_quizzes_html += "<div class='single_quiz_div'><a href='/quiz_page/" + all_quizzes_datas.id + "'><h2> Quiz Name: " + all_quizzes_datas.name + "</h2></a>"
+            all_quizzes_html += "<div class='single_quiz_div'><a href='/quiz_page/" + all_quizzes_datas.id + "'><h2>" + all_quizzes_datas.name + "</h2></a>"
             all_quizzes_html += "<h2> Total Questions:" + $(all_quizzes_datas.questions).length + "</h2>"
             var quiz_publish_time_date = new Date(all_quizzes_datas.publish_date_time);
             all_quizzes_html += "<h2> Publish Date: " + quiz_publish_time_date.toString().substring(3, 24) + "</h2>"
