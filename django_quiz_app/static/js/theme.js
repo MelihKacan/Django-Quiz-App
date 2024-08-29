9ca59bd165f997001641dc62cb4447fb51501d74
Hyper Code <meihkacan381@gmail.com>
2024-08-29 17:44:00 +0300

    All Quizzes Page White Theme Update

--- django_quiz_app/static/js/theme.js
+++ django_quiz_app/static/js/theme.js
@@ -3,7 +3,7 @@ $(function(){
         if(data.user_theme == false){}
         else{
             $(function(){
-                $( "h1, h2, h3, body, a, ul, label" ).addClass( "dark-theme" );
+                $( "h1, h2, h3, body, a, ul, label, div, #all_quizzes > div" ).addClass( "dark-theme" );
                 $( "li > a" ).addClass( "dark-theme" );
                 $( "a > h1" ).addClass( "dark-theme" );
             })
